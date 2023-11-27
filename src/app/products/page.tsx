3e2b913ae3cac5cb126c1efba7dcedba2021c9d0
unknown <chosule@naver.com>
2023-11-27 16:38:15 +0900

    feat : revalidate ISR렌더링 fetch함수에서 next키로 이용하기

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,17 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
 
-export const revalidate = 3;
+//ISR 렌더링 몇초마다 한번씩 생성가능함
+// export const revalidate = 3;
 
 export default async function ProductPage() {
-
-   const products = await getProducts();
+  const products = await getProducts();
+  const res = await fetch('https://meowfacts.herokuapp.com',{
+    //next키를 이용해서 revalidate 생성 가능 !
+    next:{ revalidate : 3}
+  });
+  const data = await res.json();
+  const factText = data.data[0];
   return (
     <>
       <h1>제품 소개페이지 !</h1>
@@ -15,6 +21,7 @@ export default async function ProductPage() {
             <Link href={`/products/${product.id}`}>{product.name}</Link>
           </li>
         ))}
+        {factText}
       </ul>
     </>
   );

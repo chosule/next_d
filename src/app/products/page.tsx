a85ad45070a5b31a1bd6d3a9629ff876a49e7c74
unknown <chosule@naver.com>
2023-11-27 16:47:27 +0900

    feat: ISR을 fetch 시에 cache옵션을 사용하여  SSR렌더링처럼 보이게 하기

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -8,7 +8,12 @@ export default async function ProductPage() {
   const products = await getProducts();
   const res = await fetch('https://meowfacts.herokuapp.com',{
     //next키를 이용해서 revalidate 생성 가능 !
-    next:{ revalidate : 3}
+    //revalidate를 0으로 설정할경우에 SSR처럼 사용가능
+    // next:{ revalidate : 3}
+
+    // cache라는 옵션을 사용해서 SSR처럼 사용가능
+    // cache를 하지않겟다고 설정함으로써 페이지가 요청이 오면 그때 fetch함
+    cache: 'no-store'
   });
   const data = await res.json();
   const factText = data.data[0];

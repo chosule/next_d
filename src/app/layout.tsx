4e17c5dc9aee6f76037a0f7fc6a639304a76a73c
chosule <89799325+chosule@users.noreply.github.com>
2023-11-28 23:15:19 +0900

    feat: next 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,43 @@
-import './globals.css'
-import styles from "@/app/layout.module.css"
-import { Metadata } from 'next'
-import Link from 'next/link'
+import "./globals.css";
+import styles from "@/app/layout.module.css";
+import { Metadata } from "next";
+import Link from "next/link";
+import { Open_Sans } from "next/font/google";
+import { Nanum_Gothic } from "next/font/google";
 
+//폰트적용
+const openSans = Open_Sans({ subsets: ["latin"] });
+const gothic = Nanum_Gothic({
+  weight: "700",
+  subsets: ["latin"],
+});
 
+export const metadata: Metadata = {
+  title: "next",
+  description: "next_study",
+};
 
-
-export const metadata:Metadata = {
-  title:'next',
-  description:'next_study'
-}
-
-const RootLayout = ({children}:{children:React.ReactNode}) => {
-
-  return(
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
     <>
-      <html lang="en">
-      <head/>
-      <body>
-        <div className={styles.bodyWrap}>
-          <header className={styles.header}>
-            <h1>Demo Note App</h1>
-            <nav className={styles.nav}>
-              <Link href="/products">Products</Link>
-              <Link href="/about">About</Link>
-              <Link href='/contact'>Contact</Link>
-            </nav>
-          </header>
-          {children}
-        </div>
-      </body>
-    </html>
-  
+      <html lang="en" className={openSans.className}>
+        <head />
+        <body>
+          <div className={styles.bodyWrap}>
+            <header className={styles.header}>
+              <h1 className={gothic.className}>Demo Note App</h1>
+              <nav className={styles.nav}>
+                <Link href="/products">Products</Link>
+                <Link href="/about">About</Link>
+                <Link href="/contact">Contact</Link>
+              </nav>
+            </header>
+            {children}
+          </div>
+        </body>
+      </html>
     </>
-   )
-}
+  );
+};
 
-
-export default RootLayout
+export default RootLayout;

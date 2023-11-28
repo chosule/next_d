e582618e4d79087ee426e3eb642d7ad52ddc5f71
chosule <89799325+chosule@users.noreply.github.com>
2023-11-28 22:32:42 +0900

    next 이미지 넣는 방법 , 가장 먼저 보여주고 싶은 이미지가 있다면 priority 라고 적어주기

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,12 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
+import cloth from "../../../public/img/clothes.jpg";
+import Image from "next/image";
 
 //ISR 렌더링 몇초마다 한번씩 생성가능함
 // export const revalidate = 3;
 
 export default async function ProductPage() {
-  throw new Error();
-
   const products = await getProducts();
   const res = await fetch("https://meowfacts.herokuapp.com", {
     //next키를 이용해서 revalidate 생성 가능 !
@@ -22,6 +22,7 @@ export default async function ProductPage() {
   return (
     <>
       <h1>제품 소개페이지 !</h1>
+      <Image src={cloth} alt="이미지" priority />
       <ul>
         {products?.map((product, index) => (
           <li key={index}>

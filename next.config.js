f0c33f97fd800223f23391ce2de8cd9c94045fcd
chosule <89799325+chosule@users.noreply.github.com>
2023-12-02 12:22:33 +0900

    redirect permenent설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/products/deleted_forever",
+        destination: "/products",
+        permanent: true, //영원히 옮겨갔다고 말해줌
+      },
+      {
+        source: "/products/deleted_temp",
+        destination: "/products",
+        permanent: false, //일시적으로 옮겨졌다고 서렂ㅇ
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/app/products/about/dreamEllie.tsx
@@ -0,0 +1,3 @@
+export default function DreamEllie() {
+  return <div> Rewrite test</div>;
+}
